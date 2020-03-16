cbdaa0f9de96151fdb1366a074ecebd4651d9604
eran <eranmir@gmail.com>
2020-03-16 15:35:40 +0200

    finished now

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ function Square(props) {
     }
   
     handleClick(i) {
-      const history = this.state.history.slice ;
+      const history = this.state.history.slice(0, this.state.stepNumber + 1) ;
       const current = history[history.length - 1];
       const squares = current.squares.slice();
       if (calculateWinner(squares) || squares[i]) {
@@ -80,7 +80,7 @@ function Square(props) {
     }
     render() {
       const history = this.state.history;
-      const current = history[history.length - 1];
+      const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
       const moves = history.map((step, move) => {
           const desc = move ?
